f30ddb9365c2f86b126090978db24a712d2a9a42
deidmon <paul.dedios@alum.udep.edu.pe>
2025-01-20 20:49:45 -0500

    Modificando el control acceso

--- src/main/java/foro/hub/api/infra/security/SecurityConfigurations.java
+++ src/main/java/foro/hub/api/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package foro.hub.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,9 +12,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfigurations {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,11 +27,14 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 ;
         return http.build();*/
-        return http
-                .csrf(AbstractHttpConfigurer::disable) // Desactiva CSRF
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Sesión sin estado
-                ).build();
+        return http.csrf(csrf -> csrf.disable()) // Desactiva CSRF
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sesión sin estado
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll() // Permite acceso a /login
+                        .anyRequest().authenticated() // Todas las demás rutas requieren autenticación
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) //valida que el usuario tenga un inicio de sesión
+                .build();
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws  Exception{
